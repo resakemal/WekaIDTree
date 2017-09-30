f27fd0666bc4a831482ec3d9df93bd5b56bc9c8f
Ahmad Idham T. Lubis <DXGeneral@users.noreply.github.com>
2017-09-30 16:18:34 +0700

    Save/Load File (.arff, .csv, .data)

--- src/TUBES/Main.java
+++ src/TUBES/Main.java
@@ -7,22 +7,13 @@ package TUBES;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.Random;
-import java.util.Scanner;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
-import weka.classifiers.bayes.NaiveBayes;
-import weka.classifiers.functions.MultilayerPerceptron;
-import weka.classifiers.lazy.IBk;
-import weka.classifiers.trees.J48;
-import weka.core.Debug;
-import weka.core.Instance;
-import weka.core.converters.ConverterUtils.DataSource;
 import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
 import weka.filters.supervised.attribute.Discretize;
-import weka.filters.unsupervised.attribute.NumericToNominal;
 
 /**
  *
@@ -34,26 +25,6 @@ public class Main {
     myID3 id3;
     Evaluation E;
         
-    
-    /**
-     * @throws java.io.IOException
-     */
-    
-//    public void loadFile() throws IOException, Exception
-//    {
-//        //ArffLoader loader = new ArffLoader();
-//        //loader.setSource(new File("iris.arff"));
-//        //Instances in = loader.getDataSet();
-//        rawData = DataSource.read("iris.arff");
-//        rawData.setClassIndex(rawData.numAttributes() - 1);
-//    }
-//    
-//    public void filter() throws Exception
-//    {
-//        Discretize D = new Discretize();
-//        D.setInputFormat(rawData);
-//        playData = Filter.useFilter(rawData, D);
-//    }
     
     public void evaluate(Classifier classifier, Instances data) throws Exception
     {
@@ -79,12 +50,45 @@ public class Main {
         System.out.println(eval.toMatrixString());
     }
     
+    public void split_test(Classifier classifier, Instances data) throws Exception
+    {
+        /* Split Test */
+        
+        data.randomize(new java.util.Random(0));
+        int trainSize = (int) Math.round(data.numInstances() * 0.8);
+        int testSize = data.numInstances() - trainSize;
+        Instances train = new Instances(data, 0, trainSize);
+        Instances test = new Instances(data, trainSize, testSize);
+        Evaluation eval = new Evaluation(test);
+        Object[] prediction = new Object[test.numInstances()];
+        myID3 cls = new myID3();
+        cls.buildClassifier(train);
+        System.out.println("check");
+        eval.evaluateModel(cls, test, prediction);
+        
+        System.out.println(eval.toSummaryString());
+        System.out.println(eval.toClassDetailsString());
+        System.out.println(eval.toMatrixString());
+    }
+    
+    // save model/hipotesis ke file external
+    public void saveModelID3(Classifier C, String dir) throws Exception
+    {
+        dir += ".model";
+        weka.core.SerializationHelper.write(dir, C);
+    }
+    
+    // load model/hipotesis dari file external
+    public static Classifier load(String dir) throws Exception {
+        return (Classifier) weka.core.SerializationHelper.read(dir);
+    }
+    
     public void prepareID3(String fileName) throws Exception
     {
         id3 = new myID3();
 
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
-        rawData = new Instances(reader);
+        DataSource source = new DataSource(fileName);
+        rawData = new Instances(source.getDataSet());
         rawData.setClassIndex(rawData.numAttributes() - 1);
 
         Discretize D = new Discretize();
@@ -99,23 +103,27 @@ public class Main {
     
     /**
      * @param args the command line arguments
+     * @throws java.lang.Exception
      */
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
         
         Main model = new Main();
         
-        String fileName = "weather.nominal.arff";
+        String fileName = "car.data"; // weather.nominal.arff buying,maint,doors,persons,lug_boot,safety
         
         System.out.println("Build ID3 with "+fileName);
         model.prepareID3(fileName); 
         System.out.println();
         
-        System.out.println("Evaluate ID3 with "+fileName);
+        System.out.println("Full Training - ID3 with "+fileName);
         model.evaluate(model.id3, model.playData);
         
-        System.out.println("Validate ID3 with "+fileName);
+        System.out.println("10-Fold Cross Validation - ID3 with "+fileName);
         model.validate(model.id3, model.playData);
+        
+        System.out.println("Split Test - ID3 with "+fileName);
+        model.split_test(model.id3, model.playData);
         
     }
     
