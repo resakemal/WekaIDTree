33d75b3ada58a28ba60e84b0a146de5cbaf0a886
JeremiaJ15 <jeremia_jason@yahoo.com>
2017-10-01 18:18:15 +0700

    check class majority

--- src/TUBES/myPrune.java
+++ src/TUBES/myPrune.java
@@ -4,14 +4,53 @@ package TUBES;
  *
  * @author JJ
  */
+import TUBES.myC45;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class myPrune {
 
-    private double getMajorityClass(myC45 subTree) {
-        
+    private ArrayList<Double> getChildClasses(myC45 subTree) {
+        // If node is not leaf, get child's class value
+        ArrayList<Double> childValues = new ArrayList<>();
+        if (subTree.main_Attribute != null) {
+            for (int i = 0; i < subTree.child_Nodes.length; i++) {
+                childValues.addAll(getChildClasses(subTree.child_Nodes[i]));
+            }
+        } else {
+            childValues.add(subTree.m_ClassValue);
+        }
+        return childValues;
+    }
+    
+    private Double getMajorityClass(myC45 subTree) {
+        ArrayList<Double> childValues = getChildClasses(subTree);
+        HashMap<Double,Integer> hm = new HashMap<Double,Integer>();
+        int max  = 1;
+        Double temp = 0.0;
+
+        for(int i = 0; i < childValues.size(); i++) {
+
+            if (hm.get(childValues.get(i)) != null) {
+
+                int count = hm.get(childValues.get(i));
+                count++;
+                hm.put(childValues.get(i), count);
+
+                if(count > max) {
+                    max  = count;
+                    temp = childValues.get(i);
+                }
+            }
+            else 
+                hm.put(childValues.get(i),1);
+        }
+        return temp;
     }
     
     private boolean isPrune(myC45 treeNow) {
-        
+
     }
 
     public void pruneTree() {
